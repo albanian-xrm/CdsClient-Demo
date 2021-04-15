14668f05742c7c42757bf15b951d22f673f6e405
Betim Beja <11160171+BetimBeja@users.noreply.github.com>
2021-04-15 15:05:57 +0200

    Updated dependencies

---
+++ AlbanianXrm.WebAPI.Integration/Controllers/CustomAPIController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.PowerPlatform.Dataverse.Client;
+using Microsoft.Xrm.Sdk;
+using System.Threading.Tasks;
+
+namespace AlbanianXrm.WebAPI.Integration.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomAPIController : ControllerBase
+    {
+        private readonly ServiceClient organizationService;
+
+        public CustomAPIController(ServiceClient organizationService)
+        {
+            this.organizationService = organizationService;
+        }
+
+        // GET: api/<controller>
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            return (await organizationService.ExecuteAsync(new OrganizationRequest("albxrm_Function"))).Results["Message"] as string;
+        }
+
+        [HttpPost]
+        public async Task<string> Post()
+        {
+            return (await organizationService.ExecuteAsync(new OrganizationRequest("albxrm_Action"))).Results["Message"] as string;
+        }
+    }
+}
